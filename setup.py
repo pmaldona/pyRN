68b529dcdaa55564c07ff4ebcc409f0ad2e031f0
Fionn Keogh <fionndaire.keogh@outlook.de>
2022-04-12 10:33:32 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -32,5 +32,5 @@ setup(
     packages=find_packages(exclude=['tests']),
 
     # run-time dependencies that will be installed by pip
-    install_requires=['numpy','pandas','BeautifulSoup4', 'bitarray','scipy','networkx']
+    install_requires=['numpy','pandas','BeautifulSoup4', 'bitarray','scipy','networkx', 'pypoman']
 )
