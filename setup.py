3be77762c45a82063dafd7993ba6e97befa6121e
Fionn Keogh <fionndaire.keogh@outlook.de>
2023-03-07 10:57:42 +0100

    Added Joblib
    Added Joblib so build works again

--- setup.py
+++ setup.py
@@ -31,5 +31,5 @@ setup(
     packages=find_packages(exclude=['tests']),
 
     # run-time dependencies that will be installed by pip
-    install_requires=['numpy','pandas','beautifulsoup4', 'bitarray','scipy','networkx','libroadrunner','lxml','pyvis','pypoman','matplotlib']
+    install_requires=['numpy','pandas','beautifulsoup4', 'bitarray','scipy','networkx','libroadrunner','lxml','pyvis','pypoman','matplotlib', 'joblib']
 )
